243433af31cb7c4132e6194900d1f286db9b831e
solarezz <122013283+solarezz@users.noreply.github.com>
2024-07-30 18:21:02 +0500

    yandex gpt in my tg

---
+++ gpt.py
@@ -0,0 +1,38 @@
+import asyncio
+from telethon import TelegramClient, events
+from yandexgptlite import YandexGPTLite
+
+account = YandexGPTLite('b1gck5hl5v4ktgcnlv3m', 'y0_AgAAAABziHH_AATuwQAAAAEMA-UkAACDH9ws6mdEYL9rQubGdlGGpIe4xw' )
+
+
+api_id = 23966203
+api_hash = '85bd96f12b78d9140335ca52ce75120a'
+
+client = TelegramClient('gpt_from_my_tg', api_id, api_hash)
+
+
+@client.on(events.NewMessage)
+async def handler(event):
+    print(f'Новое сообщение: {event.raw_text}')
+
+@client.on(events.NewMessage(pattern='/gpt (.+)'))
+async def gpt_command(event):
+    user_text = event.pattern_match.group(1)  # Получаем текст после команды
+    text = account.create_completion(prompt=user_text, temperature='0.6')
+
+
+    await event.respond(text)
+
+async def main():
+    await client.start()
+    print('Client started. GO!')
+
+    #await client.send_message('me', 'Тестовое сообщение')
+
+    await client.run_until_disconnected()
+
+
+asyncio.run(main())
+
+with client:
+    client.loop.run_until_complete(main())
